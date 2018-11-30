7fa069e2ee4110e44dbc7970edc87ba96092081d
Andrey Pechkurov <andrey.pechkurov@dataart.com>
2018-11-30 13:40:32 +0300

    Initial draft

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # express-rtracer
-Express middleware for CLS-based request id generation, batteries included
+
+Express middleware for CLS-based request id generation, batteries included.
+
+Generates a UUID value as the id for each request. If the request contains `X-Request-Id` header, uses its value instead.
+
+WIP at the moment
---
+++ index.js
@@ -0,0 +1,46 @@
+'use strict'
+
+const cls = require('cls-hooked')
+const uuidv4 = require('uuid/v4')
+
+// generate a unique value for namespace
+const nsid = `rtracer:${uuidv4()}`
+const ns = cls.createNamespace(nsid)
+
+/**
+ * Generates a request tracer middleware.
+ * @param {Object} options possible options
+ * @param {boolean} options.useHeader respect request header flag (default: `true`)
+ * @param {string} options.headerName request header name (used if `useHeader` set to `true`; default: `X-Request-Id`)
+ */
+const middleware = ({
+  useHeader = true,
+  headerName = 'X-Request-Id'
+} = {}) => {
+  return (req, _, next) => {
+    let requestId
+    if (useHeader) {
+      requestId = req.headers[headerName.toLowerCase()]
+    }
+    requestId = requestId || uuidv4()
+
+    ns.run(() => {
+      ns.set('requestId', requestId)
+      next()
+    })
+  }
+}
+
+/**
+ * Returns request tracer id or `undefined` in case if the call is made from an outside CLS context.
+ */
+const id = () => {
+  if (ns && ns.active) {
+    return ns.get('requestId')
+  }
+}
+
+module.exports = {
+  middleware,
+  id
+}
